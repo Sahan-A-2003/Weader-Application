fbc4a7182ac945c09e90aa9a73a71518bf06294a
Sahan Asantha <sahan.asantha2003@gmail.com>
2024-10-17 10:49:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+
+const weaderAPI = "c2c5eda2b5c7b98ccc49235289e97cb0";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+let seachBox = document.querySelector(".seach-contry");
+const seachBtn = document.querySelector(".seach button");
+const weaderImage = document.querySelector(".weather-image");
+
+async function chackWeader(city){
+  // fetch the weader API.
+  const respons = await fetch(apiURL + city + `&appid=${weaderAPI}`);
+
+  if (respons.status == 404) {
+    document.querySelector(".weather-display").style.display = "none";
+    document.querySelector(".invalid-input").style.display = "block";
+
+  }else{
+
+    var data = await respons.json();
+    let weader = data.weather[0].main;
+
+    document.querySelector(".Country").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector("#Hpresanteg").innerHTML = data.main.humidity + "%";
+    document.querySelector("#wSpeed").innerHTML = data.wind.speed.toFixed(1) + "Km\\h";
+
+    // Change images acoding to the weader
+
+    if(weader == "Clouds"){
+      weaderImage.src = "images/clouds.png";
+
+    }else if(weader == "Clear"){
+      weaderImage.src = "images/clear.png";
+
+    }else if(weader == "Mist"){
+      weaderImage.src = "images/mist.png";
+
+    }else if(weader == "Rain"){
+      weaderImage.src = "images/rain.png";
+
+    }else if(weader == "Drizzle"){
+      weaderImage.src = "images/drizzle.png";
+
+    }else if(weader == "Snow"){
+      weaderImage.src = "images/snow.png";
+
+    }
+
+    document.querySelector(".weather-display").style.display = "block";
+    document.querySelector(".invalid-input").style.display = "none";
+    seachBox.value = "";
+
+    }
+
+}
+// seach button click event.
+seachBtn.addEventListener("click", ()=>{
+  chackWeader(seachBox.value);
+})
